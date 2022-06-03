7d6af65d812be2552d63254d083253fff9ce4877
vijaykumar <vijay1111mane@gmail.com>
2022-06-03 17:42:56 +0530

    commit;

---
+++ src/components/Child.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- <h1>{{data}}</h1> -->
+    <slot><h1>Child components</h1></slot>
+</template>
+
+<script>
+export default {
+  name: "Child-page",
+//   props:{ 
+//       data:String
+//   }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Slots in vue js</h1>
+    <Child></Child>
+    <!-- <Child data="vijay mane"/> -->
+    <Child><h1>Welcome to Bangalore</h1></Child>
+    <Child><a href="">Welcome</a></Child>
+    
+    <Child><img src="../assets/download.png" ></Child>
+    
+</template>
+
+<script>
+
+import Child from './Child.vue'
+
+export default{
+    name: "Home-page",
+    components:
+    {
+    Child,
+  
+}
+}
+</script>
+
+<style>
+h1{
+    color: orange;
+}
+</style>
