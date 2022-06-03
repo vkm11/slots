5e2502e08df2996e3cd86582dda853b7105e0e9f
vijaykumar <vijay1111mane@gmail.com>
2022-06-03 18:08:32 +0530

    update

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,13 +1,37 @@
 <template>
-    <!-- <h1>{{data}}</h1> -->
-    <slot><h1>Child components</h1></slot>
+  <!-- <h1>{{data}}</h1> -->
+  <!-- <slot><h1>Child components</h1></slot> -->
+  <!-- <slot><h1>Default Data</h1></slot>
+    <slot><h1>Default Data</h1></slot>
+    <slot><h1>Default Data</h1></slot> -->
+
+  <!-- multiple slots -->
+  <div class="divs">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <content>
+      <slot name="main"></slot>
+    </content>
+    <footer>
+      <slot name="footer"><button>Click me</button></slot>
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Child-page",
-//   props:{ 
-//       data:String
-//   }
-}
+  //   props:{
+  //       data:String
+  //   }
+};
 </script>
+<style scoped>
+.divs{
+    border: 1px solid;
+    width: 170px;
+    text-align: center;
+    padding:20px
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,32 @@
 <template>
-    <h1>Slots in vue js</h1>
-    <Child></Child>
+    <Child>
+        <template v-slot:header> 
+            <h1>Vuejs</h1>
+        </template>
+        <template v-slot:main> 
+            <p>A vue js product</p>
+        </template>
+        <template v-slot:footer> 
+            <button>Buy now</button>
+        </template>
+    </Child>
+     <Child>
+        <template v-slot:header> 
+            <h1>Angular</h1>
+        </template>
+        <template v-slot:main> 
+            <p>A Angularproduct</p>
+        </template>
+        <template v-slot:footer> 
+            <!-- <button>Buy now</button> -->
+        </template>
+    </Child>
     <!-- <Child data="vijay mane"/> -->
-    <Child><h1>Welcome to Bangalore</h1></Child>
+    <!-- <Child><h1>Welcome to Bangalore</h1></Child>
     <Child><a href="">Welcome</a></Child>
     
     <Child><img src="../assets/download.png" ></Child>
-    
+     -->
 </template>
 
 <script>
